46e2a97da6f25c28ab1f370d30b9cde551da5cab
barissekerciler <sekercilerbaris@gmail.com>
2019-02-20 00:30:52 +0300

    removed exceptions

--- db_operations.py
+++ db_operations.py
@@ -11,40 +11,34 @@ class DbOperations(object):
         return normalized_result
 
     def insert_message_to_db(self, username, message):
-        try:
-            db = pymysql.connect(host=os.environ.get('MYSQLHOST'), user=os.environ.get('MYSQLUSERNAME'),
-                                 passwd=os.environ.get('MYSQLPASSWORD'),
-                                 db=os.environ.get('MYSQLDB'), port=int(os.environ.get('MYSQLPORT')))
-            cursor = db.cursor()
-            cursor.execute(
-                "INSERT INTO messages(username, message) VALUES('{username}', '{message}')".format(username=username,
-                                                                                                   message=message))
+        db = pymysql.connect(host=os.environ.get('MYSQLHOST'), user=os.environ.get('MYSQLUSERNAME'),
+                             passwd=os.environ.get('MYSQLPASSWORD'),
+                             db=os.environ.get('MYSQLDB'), port=int(os.environ.get('MYSQLPORT')))
+        cursor = db.cursor()
+        cursor.execute(
+            "INSERT INTO messages(username, message) VALUES('{username}', '{message}')".format(username=username,
+                                                                                               message=message))
 
-            cursor.execute('SELECT last_insert_id()')
-            last_id = cursor.fetchone()
-            cursor.execute(
-                "SELECT username, message FROM messages WHERE message_id = '{last_id}'".format(last_id=last_id[0]))
-            columns = [column[0] for column in cursor.description]
-            payload = cursor.fetchall()
-            cursor.close()
-            db.commit()
-            db.close()
-            return self.query_response_normalizer(columns, payload)
-        except Exception('could not insert'):
-            raise
+        cursor.execute('SELECT last_insert_id()')
+        last_id = cursor.fetchone()
+        cursor.execute(
+            "SELECT username, message FROM messages WHERE message_id = '{last_id}'".format(last_id=last_id[0]))
+        columns = [column[0] for column in cursor.description]
+        payload = cursor.fetchall()
+        cursor.close()
+        db.commit()
+        db.close()
+        return self.query_response_normalizer(columns, payload)
 
     def get_data(self):
-        try:
-            db = pymysql.connect(os.environ.get('MYSQLHOST'), os.environ.get('MYSQLUSERNAME'),
-                                 os.environ.get('MYSQLPASSWORD'),
-                                 os.environ.get('MYSQLDB'))
-            cursor = db.cursor()
-            cursor.execute('SELECT username, message FROM messages LIMIT 50')
-            columns = [column[0] for column in cursor.description]
-            payload = cursor.fetchall()
-            cursor.close()
-            db.commit()
-            db.close()
-            return self.query_response_normalizer(columns, payload)
-        except Exception('could not get'):
-            raise
+        db = pymysql.connect(os.environ.get('MYSQLHOST'), os.environ.get('MYSQLUSERNAME'),
+                             os.environ.get('MYSQLPASSWORD'),
+                             os.environ.get('MYSQLDB'))
+        cursor = db.cursor()
+        cursor.execute('SELECT username, message FROM messages LIMIT 50')
+        columns = [column[0] for column in cursor.description]
+        payload = cursor.fetchall()
+        cursor.close()
+        db.commit()
+        db.close()
+        return self.query_response_normalizer(columns, payload)
